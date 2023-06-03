6c4e6690c4e43e909641fe0422ff7d2b76047fdc
Melvin <melvinhkusuma@gmail.com>
2023-06-03 18:13:05 +1000

    title animation when links pressed

--- content_script.js
+++ content_script.js
@@ -1,27 +1,32 @@
 const digitalArt = document.getElementById('digital-art');
 const programming = document.getElementById('programming');
 const about = document.getElementById('about');
-const html = document.documentElement;
+const titleWrapper = document.getElementById('title-wrapper');
 
 function checkHash() {
-    html.style.setProperty('align-items', 'center');
-    digitalArt.style.setProperty('display', 'none');
-    programming.style.setProperty('display', 'none');
-    about.style.setProperty('display', 'none');
+    digitalArt.style.display = "none";
+    programming.style.display = "none";
+    about.style.display = "none";
 
     if (location.hash === '#digital_art' || location.hash === '#programming' || location.hash === '#about') {
-        html.style.setProperty('align-items', 'initial');
+        titleWrapper.style.top = "0%";
+        titleWrapper.style.left = "50%";
+        titleWrapper.style.transform = "translate(-50%, -0%)";
         switch (location.hash) {
             case '#digital_art':
-                digitalArt.style.setProperty('display', 'initial');
+                digitalArt.style.display = "initial";
                 break;
             case '#programming':
-                programming.style.setProperty('display', 'initial');
+                programming.style.display = "initial";
                 break;
             case '#about':
-                about.style.setProperty('display', 'initial');
+                about.style.display = "initial";
                 break;
         }
+    } else if (location.hash === "") {
+        titleWrapper.style.top = "50%";
+        titleWrapper.style.left = "50%";
+        titleWrapper.style.transform = "translate(-50%, -50%)";
     }
 }
 
