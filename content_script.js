da3d2939a1ae4bb625da2a9c84e6771df633d4ef
Melvin <melvinhkusuma@gmail.com>
2023-06-03 22:15:40 +1000

    content container is in place

--- content_script.js
+++ content_script.js
@@ -4,9 +4,9 @@ const about = document.getElementById('about');
 const titleWrapper = document.getElementById('title-wrapper');
 
 function checkHash() {
-    digitalArt.style.display = "none";
-    programming.style.display = "none";
-    about.style.display = "none";
+    digitalArt.style.opacity = "0";
+    programming.style.opacity = "0";
+    about.style.opacity = "0";
 
     if (location.hash === '#digital_art' || location.hash === '#programming' || location.hash === '#about') {
         titleWrapper.style.top = "0%";
@@ -14,13 +14,13 @@ function checkHash() {
         titleWrapper.style.transform = "translate(-50%, -0%)";
         switch (location.hash) {
             case '#digital_art':
-                digitalArt.style.display = "initial";
+                digitalArt.style.opacity = "1";
                 break;
             case '#programming':
-                programming.style.display = "initial";
+                programming.style.opacity = "1";
                 break;
             case '#about':
-                about.style.display = "initial";
+                about.style.opacity = "1";
                 break;
         }
     } else if (location.hash === "") {
