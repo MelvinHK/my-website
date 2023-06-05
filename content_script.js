9f3dbcff044286921f3572c5a5247a76e369ce82
Melvin <melvinhkusuma@gmail.com>
2023-06-05 17:00:50 +1000

    viewport scrollbar, fixed canvas position

--- content_script.js
+++ content_script.js
@@ -3,18 +3,22 @@ const programming = document.getElementById('programming');
 const about = document.getElementById('about');
 const titleWrapper = document.getElementById('title-wrapper');
 const contentContainer = document.getElementById('content-container');
+const html = document.documentElement;
 
 function checkHash() {
     digitalArt.style.opacity = "0";
     programming.style.opacity = "0";
     about.style.opacity = "0";
+    html.style.overflowY = "hidden";
 
     if (location.hash === '#digital_art' || location.hash === '#programming' || location.hash === '#about') {
+        window.scrollTo(0, 0);
         titleWrapper.style.top = "0%";
         titleWrapper.style.left = "50%";
         titleWrapper.style.transform = "translate(-50%, -0%)";
         titleWrapper.style.transform += "scale(0.75)";
         contentContainer.style.top = "0%";
+        html.style.overflowY = "auto";
         switch (location.hash) {
             case '#digital_art':
                 digitalArt.style.opacity = "1";
