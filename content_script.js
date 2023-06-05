2848179ebb364bdb7fc17b2a10e4d3c7ed6f8ece
Melvin <melvinhkusuma@gmail.com>
2023-06-05 16:02:05 +1000

    content animation, white spacer

--- content_script.js
+++ content_script.js
@@ -2,6 +2,7 @@ const digitalArt = document.getElementById('digital-art');
 const programming = document.getElementById('programming');
 const about = document.getElementById('about');
 const titleWrapper = document.getElementById('title-wrapper');
+const contentContainer = document.getElementById('content-container');
 
 function checkHash() {
     digitalArt.style.opacity = "0";
@@ -12,6 +13,8 @@ function checkHash() {
         titleWrapper.style.top = "0%";
         titleWrapper.style.left = "50%";
         titleWrapper.style.transform = "translate(-50%, -0%)";
+        titleWrapper.style.transform += "scale(0.75)";
+        contentContainer.style.top = "0%";
         switch (location.hash) {
             case '#digital_art':
                 digitalArt.style.opacity = "1";
@@ -27,6 +30,7 @@ function checkHash() {
         titleWrapper.style.top = "50%";
         titleWrapper.style.left = "50%";
         titleWrapper.style.transform = "translate(-50%, -50%)";
+        contentContainer.style.top = "47%";
     }
 }
 
