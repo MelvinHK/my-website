21680fbdb84da9a67950feeb29886a9a188d5ec2
Melvin <melvinhkusuma@gmail.com>
2023-06-13 16:17:48 +1000

    about page, title animation cancellation

--- content_script.js
+++ content_script.js
@@ -2,10 +2,11 @@ const html = document.documentElement;
 
 const titleWrapper = document.getElementById('title-wrapper');
 const titleContainer = document.getElementById('title-container');
+const menuList = document.getElementById('menu-list');
 
 const contentContainer = document.getElementById('content-container');
 
-const pages = {
+const pages = { // Keys must match corresponding href in index.html
     '#digital_art': document.getElementById('digital-art'),
     '#programming': document.getElementById('programming'),
     '#about': document.getElementById('about')
@@ -28,6 +29,7 @@ function checkHash() {
             if (page != location.hash)
                 pages[page].style.display = "none";
     } else {
+        titleWrapper.style.transitionDuration = '1s';
         titleWrapper.classList.remove('title-top');
         contentContainer.style.top = "47%";
         html.style.overflowY = "hidden";
@@ -35,6 +37,19 @@ function checkHash() {
         for (const page in pages)
             pages[page].style.display = 'initial';
     }
+}
+
+function cancelAnimation() {
+    titleContainer.style.animation = 'none';
+    titleWrapper.style.transitionDuration = '0s';
+
+    for (let i = 0; i < menuList.children.length; i++)
+        menuList.children[i].style.animation = 'none';
+}
+
+// Cancel title intro animations if user refreshes/deep links hash page
+if (Object.keys(pages).includes(location.hash)) {
+    cancelAnimation();
 }
 
 checkHash();
